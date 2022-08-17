baeb1ac48fa81d57e6bee286e82d76a029d84c31
heimanba <yuankun.yk@alibaba-inc.com>
2022-08-17 16:00:28 +0800

    add lib

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {run} from './handle'
 function exec(): void {
   const token = core.getInput('github-token')
   // const token = process.env['INPUT_GITHUB_TOKEN'] as string
+  core.info(`env: ${JSON.stringify(process.env, null, 2)}`)
   run(token)
 }
 
