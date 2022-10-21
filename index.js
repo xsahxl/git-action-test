f10dd3f577de0bd7b7c72a562b4b0ad0be4caa6c
wss-git <wssgryx@163.com>
2022-10-21 16:37:54 +0800

    worker update app ots support complete

---
+++ index.js
@@ -0,0 +1,14 @@
+// const logger = require('logger');
+const Engine = require('logger');
+
+
+const app1 = { a: 'cc', b: 'bb' };
+
+const log = new Engine({
+  level: 'debug',
+  secret: Object.values(app1),
+});
+
+log.info(JSON.stringify({ a: 'cc', b: 'bb', c: 'ssss' }));
+
+
