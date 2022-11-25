9524f863707a7b60e5e58d35bfced4a1f5e4644f
wss-git <wssgryx@163.com>
2022-11-25 13:52:05 +0800

    test

---
+++ index.js
@@ -0,0 +1,14 @@
+const { URL } = require('url')
+
+const a = (url) => {
+  const parsed = new URL(url)
+  console.log('parsed:: ', parsed);
+  const from = `${parsed.protocol}//${parsed.host}${parsed.pathname}`
+  const rel = new URL('.', from)
+  console.log('rel: ', rel);
+  const res = `//${rel.host}${rel.pathname}`
+  console.log('res: ', res);
+  return res
+}
+
+console.log(a('https://registry.npmjs.org/:_auth=xxxx'));
