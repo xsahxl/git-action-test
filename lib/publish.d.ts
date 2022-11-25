396bac3e560df5b5aed390bf98a562dd15f0b7b9
wss-git <wssgryx@163.com>
2022-11-25 17:38:35 +0800

    1.0.11 3

--- lib/publish.d.ts
+++ lib/publish.d.ts
@@ -2,14 +2,11 @@ import { Logger } from '@serverless-cd/core';
 export interface IProps {
     registry?: string;
     token?: string;
-    username?: string;
-    password?: string;
+    codeDir?: string;
 }
 export default class Publish {
     private registry;
     private token;
-    private username;
-    private password;
     private codeDir;
     logger: Logger;
     constructor(props: IProps, logger: Logger);
