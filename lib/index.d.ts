cfb2cf08b7cf434ccab3a4c467c7c8edd1bf1d4a
wss-git <wssgryx@163.com>
2022-12-09 13:24:09 +0800

    test tag

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Logger } from '@serverless-cd/core';
-import Publish from './publish';
-export declare const run: (inputs: Record<string, any>, context: Record<string, any>, logger: Logger) => Promise<void>;
-export default Publish;
