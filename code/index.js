de471837e51a5b9e8ccbadc71371d615df3ae429
wss-git <wssgryx@163.com>
2022-12-15 18:05:45 +0800

    debug 4

--- code/index.js
+++ code/index.js
@@ -4,22 +4,22 @@ exports.handler = (event, context, callback) => {
   callback(null, 'hello!' + process.cwd());
 }
 
-const npa = require('npm-package-arg')
+// const npa = require('npm-package-arg')
 
-function pickRegistry(spec, opts = {}) {
-  spec = npa(spec)
-  let registry = spec.scope && opts[spec.scope.replace(/^@?/, '@') + ':registry']
+// function pickRegistry(spec, opts = {}) {
+//   spec = npa(spec)
+//   let registry = spec.scope && opts[spec.scope.replace(/^@?/, '@') + ':registry']
 
-  if (!registry && opts.scope) {
-    registry = opts[opts.scope.replace(/^@?/, '@') + ':registry']
-  }
+//   if (!registry && opts.scope) {
+//     registry = opts[opts.scope.replace(/^@?/, '@') + ':registry']
+//   }
 
-  if (!registry) {
-    registry = opts.registry || defaultOpts.registry
-  }
+//   if (!registry) {
+//     registry = opts.registry || defaultOpts.registry
+//   }
 
 
-  return registry
-}
+//   return registry
+// }
 
-pickRegistry('@wssgryx/npm-test@1.0.5')
+// pickRegistry('@wssgryx/npm-test@1.0.5')
